7db2fe492505f59c84138d59a4eae7424a0eb6d6
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-26 11:38:11 +0530

    Add mutable immutable

---
+++ 01_basics/mutable_immutable.py
@@ -0,0 +1,16 @@
+username = "Abhi"
+print(username)
+
+username = "Abhishek"
+print(username)
+
+score = 10
+scoreOne = score
+
+print(score)
+print(scoreOne)
+
+score = 15
+print(score)
+print(scoreOne)
+
