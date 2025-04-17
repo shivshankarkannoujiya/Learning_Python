bfa668e947a714419ffab21ed21c0ec9c92cecb6
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-04-18 04:12:22 +0530

    add numbers

---
+++ 02_Numbers/one.py
@@ -0,0 +1,44 @@
+x = 1
+y = 2
+z = 3
+
+# Remainer
+print(z % 2)
+
+# Power
+print(2**3)
+
+# TODO: Study
+print(repr("study"))
+print(str("study"))
+print(("study"))
+
+print(1 > 2)
+print(1 != 2)
+print(1 == 2)
+print(x < y < z)
+
+# logical
+print(x < y and y < z)
+print(x < y or y < z)
+
+print(1 == 2 > 3)
+print(1 == 2 and 2 > 3)
+
+# Using library
+import math
+
+# floor: Return Closest number below value
+print(math.floor(3.9))
+print(math.floor(-3.9))
+
+# trunc: Towards => 0
+math.trunc(2.8)
+math.trunc(-2.8)
+
+print(math.ceil(2.9))
+print(math.ceil(-2.9))
+
+# Result: ((2 * 3) + (1*3j))
+print((2 + 1j) * 3)
+
---
+++ 02_Numbers/two.py
@@ -0,0 +1,68 @@
+# Octal Value: 0o<value>
+print(0o20)
+
+# Hex Value: 0x<value>
+print(0xFF)
+
+# Binary: 0b<value>
+print(0b10000)
+
+# CONVERSION: 
+
+# Decimal => Octal
+print(oct(64))
+
+# Decimal => Hex
+print(hex(64))
+
+# Decimal => Binary
+print(bin(10))
+
+
+# IMPORTANT
+print(int('1000',2))
+print(int('12',8))
+print(int('12',16))
+
+# bitwise
+print(4 << 2)
+print(4 >> 2)
+
+
+# import
+import random 
+print(random.random())
+print(random.randint(1, 100))
+
+teas = ["lemon", "masala", "earl gray"]
+print(random.choice(teas))
+print(random.shuffle(teas))
+
+
+# Decimal precision
+print(0.1 + 0.1 + 0.4)
+print(0.1 + 0.1 + 0.1)
+print(0.1 + 0.1 + 0.1 - 0.3) # give problem
+print((0.1 + 0.1 + 0.1) - 0.3) # give problem
+
+# Decimal Context Manager
+from decimal import Decimal
+print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1'))
+print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
+print((Decimal('0.1') + Decimal('0.1') + Decimal('0.1')) - Decimal('0.3'))
+
+# Fraction
+from fractions import Fraction
+myFraction = Fraction(2/7)
+print(myFraction)
+
+
+# SET: get => Union(|), Intersection(&), defferences(-), Superset 
+setone = {1,2,3,4}
+
+print(setone & {1,2,3})
+print(setone | {1,2,3})
+print(setone - {1,2,3,4}) # set() => {} will not come, Bcz: {} disctionary hote hai
+# set(): Empty set
+
+print(type({}))
