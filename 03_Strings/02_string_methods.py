1927e64231948b230a1bd6e4c2ed010ee9cce029
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-05-10 16:46:06 +0530

    Add python string methods

---
+++ 03_Strings/02_string_methods.py
@@ -0,0 +1,18 @@
+chai = "Masala Chai"
+
+# Methods
+# 1. lower(), upper()
+
+print(chai.lower())
+print(chai.upper())
+
+
+# 2. trim the spaces
+print(chai.strip())
+
+
+# 3. replace: Masala => Ginger
+print(chai.replace("Masala", "Ginger"))
+
+
+
